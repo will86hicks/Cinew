373def626b51035f42af3ffcc2d8744de4308d16
Will Hicks <will86hicks@gmail.com>
2015-04-10 22:02:13 -0500

    Deleting showings table, importing erm pdf, and for Jacob, nwc.load

---
+++ seeall.sql
@@ -0,0 +1,21 @@
+#Show the contents of all the tables
+select 'Members';
+select * from member;
+
+select 'Theaters';
+select * from theater;
+
+select 'Memberships';
+select * from membership;
+
+select 'Showtimes';
+select * from play;
+
+select 'Reservations';
+select * from reservation;
+
+select 'Movies';
+select * from movie;
+
+select 'Reservation seating assignments';
+select * from res_seat_assignments;
