da210a26819b9c16fa2dcd7e4ce6b806719dad27
will86hicks <will86hicks@gmail.com>
2015-04-26 22:42:21 -0500

    Adam's new files for Add/delete cinplexes and current work done on add reservation

--- website/AdminAddToReservation.php
+++ website/AdminAddToReservation.php
@@ -1,3 +1,7 @@
+
+
+
+
 <?php
 include "login.php";
 
@@ -11,11 +15,50 @@ echo
 
 <html>
 <body>
+<head>
+	<script>
+		function foo(cinplex){
+			var xmlhttp;
+
+			if(cinplex == ""){
+				//document.getElementById("demo").innerHTML = "";
+				return;
+			}
+			else{
+				
+				if(window.XMLHttpRequest)
+					{
+						//document.getElementById("demo").innerHTML = "fuck yeah";
+						xmlhttp = new XMLHttpRequest();
+					}
+				else
+				{
+					xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+			
+				xmlhttp.onreadystatechange = function(){
+					
+					if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
+					{
+						document.getElementById("demo").innerHTML=xmlhttp.responseText;
+					}
+				}
+			xmlhttp.open("GET", "getCinplex.php?q=" + cinplex, true);
+			xmlhttp.send();
+			
+			//document.getElementById("demo").innerHTML =cinplex;
+		}
+	}
+	</script>
+</head>
 
 <h1 align = "center">Add to Reservation</h1>
 
 <form action = "AdminResult_AddToReservation" method = "POST">
-	Cinema Complex: <br><select id = "name = "cinplex_name">
+	Cinema Complex: <br>
+	
+	<select id = "cinplex" onchange = "foo(this.value)" >
+			<option value ="">Select a Cineplex </option>
 			<?php
 				$result = mysql_query("SELECT c.name FROM cinplex c");
 				
@@ -25,6 +68,13 @@ echo
 						
 			?>
 	</select>
+	
+
+	
+	
+	
+	
+	<p id = "demo"></p>
 	<br><br>
 	Showtime: <br><input type = "datetime" name = "showtime">
 
